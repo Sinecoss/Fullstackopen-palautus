c3fd906d00c280300001c773bb615d7ea75ee38f
Theal Altake <zufa.altake@gmail.com>
2026-04-16 12:48:53 +0300

    1.6: unicafe step1

---
+++ osa1/unicafe/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+const Display = ({ good, neutral, bad }) => {
+  return (
+    <div>
+      <h2>statistics</h2>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+    </div>
+  )
+}
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const App = () => {
+
+  const feedback = 'give feedback'
+
+
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const setToGood = () => {
+    setGood(good + 1)
+  }
+
+  const setToNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const setToBad = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h1>{feedback}</h1>
+      <Button onClick={setToGood} text={'good'} />
+      <Button onClick={setToNeutral} text={'neutral'} />
+      <Button onClick={setToBad} text={'bad'} />
+      <Display good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
